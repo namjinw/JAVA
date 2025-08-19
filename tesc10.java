40810cec43c432b3afede5c22abb64269c8cb744
namjinw <0812jinwoo@gmail.com>
2025-08-19 18:39:13 +0900

    Add files via upload

---
+++ tesc10.java
@@ -0,0 +1,15 @@
+package codingtest;
+
+public class tesc10 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i=0; i<10; i++) {
+			if(i % 2 == 0) {
+				sum = sum + i;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ test10.java
@@ -0,0 +1,11 @@
+package codingtest;
+
+public class test10 {
+
+	public static void main(String[] args) {
+		String str = ("HRDK" + 40 + 23);
+		System.out.println(str);
+
+	}
+
+}
