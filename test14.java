343427787e2ea67b62fcd88cde7146cef7d1df00
namjinw <0812jinwoo@gmail.com>
2025-08-22 08:33:39 +0900

    Add files via upload

---
+++ test14.java
@@ -0,0 +1,16 @@
+package codingtest;
+
+public class test14 {
+	static int x = 5;
+	public static void main(String[] args) {
+		int localint = 3;
+		double b = localint;
+		int i = (int) b;
+		
+		System.out.println(x);
+		System.out.println(localint);
+		System.out.println(b);
+		System.out.println(i);
+	}
+
+}
